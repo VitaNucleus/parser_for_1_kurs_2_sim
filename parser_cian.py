aaf51c051a4bc95a7b9ed00de3447bcaf9802e2f
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2024-03-06 21:45:42 +0500

    06.03.2024 D В целом парсинг всех объявлений готов, но есть огрехи в виде того, что парсер может или перебрать или не добрать объявлений

--- parser_cian.py
+++ parser_cian.py
@@ -2,36 +2,53 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-urls = ['https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',]
-        # 'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
-        # '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
-        # 'https://perm.cian.ru/snyat-kommercheskiy-uchastok/']
+urls = ['https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&'
+        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',
+        'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
+        '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
+        'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
+        'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
 
 def get_next_page(list_li, num):
     for li in list_li:
         if li.text == f'{num}':
-            return li['href']
+            href = li.a['href']
+            if href[0] != '/':
+                href = href[20::]
+            return 'https://perm.cian.ru' + href
     return None
 
-# TODO Сделать парсинг всех объявлений, а не только одной страницы
-def parse_page(url, num = 2):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        next_url = None
-        for div in soup.find_all('div'):
-            if 'data-name' in list(div.attrs):
-                if div['data-name'] == 'CommercialOfferCard':
-                    pass
+# TODO продумать логику сбора данных, так как пока, что она не точна
+def parse_page(url, i, res, num = 2):
+    while True:
+        response = requests.get(url)
+        if response.status_code == 200:
+            break
 
-                if div['data-name'] == 'Pagination':
-                    next_url = get_next_page(div.find_all_next('li'), num)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    next_url = None
+
+    for div in soup.find_all('div', {'data-name': 'CommercialOfferCard',
+                                     'class': '_32bbee5fda--offer-container--Zhu18'}):
+
+        # if 'class' in list(div.attrs):
+        #     if div['class'] and div['class'][0] == "_32bbee5fda--container--N0wtY":
+        #         res[f'{i}'] = div.text
+        i += 1
+    print(i)
+    div_pagination = soup.find('div', {'data-name': 'Pagination'})
+    next_url = get_next_page(div_pagination.find_all_next('li'), num)
+    if next_url:
+        parse_page(next_url, i, res, num + 1)
 
 
 def main_function():
     num = 1
     for url in urls:
-        parse_page(url)
+        i = 1
+        res = {}
+        print(url)
+        parse_page(url, i, res)
         num += 1
 
 
