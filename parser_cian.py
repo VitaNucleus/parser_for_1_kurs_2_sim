9336b0ee5a165f5b8d04ee0cb2e2368fd49a0491
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2024-03-29 19:50:56 +0500

    29.03.2024 D тестовый

--- parser_cian.py
+++ parser_cian.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 import json
 
 urls = ['https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&'
-        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',
-        'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
-        '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
-        'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
-        'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
+        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',]
+        # 'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
+        # '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
+        # 'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
+        # 'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
 
 def get_next_page(list_li, num):
     for li in list_li:
@@ -31,11 +31,9 @@ def parse_page(url, i, res, num = 2):
     for div in main_div.find_all('div', {'data-name': 'CommercialOfferCard',
                                      'class': '_32bbee5fda--offer-container--Zhu18',
                                      }):
-        # if 'class' in list(div.attrs):
-        #     if div['class'] and div['class'][0] == "_32bbee5fda--container--N0wtY":
-        #         res[f'{i}'] = div.text
+        a = div.find('a', {'data-name': 'CommercialTitle'})
         i += 1
-    print(i)
+        print(a)
     div_pagination = soup.find('div', {'data-name': 'Pagination'})
     next_url = get_next_page(div_pagination.find_all_next('li'), num)
     if next_url:
