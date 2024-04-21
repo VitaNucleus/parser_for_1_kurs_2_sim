961c85455a3758da29210e96222e79a49bfe27c6
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2024-04-21 19:56:27 +0500

    21.04.2024 D Доработать парсинг цены

--- parser_cian.py
+++ parser_cian.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import json
+import os
 
-
+load_dotenv()
+# Урлы, по которым будет производиться поиск адресов с параметрами
 urls = ['https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&'
-        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',
-        'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
-        '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
-        'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
-        'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
+        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',]
+        # 'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
+        # '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
+        # 'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
+        # 'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
 
 
 def get_next_page(list_li, num):
+    """Функция возвращает урл, который будет следующем для парсинга
+
+       Сперва проверяется есть ли последующие страницы, которые надо пропарсить. Проверка проходит по числу, которое
+       передаётся в функцию"""
     for li in list_li:
         if li.text == f'{num}':
             href = li.a['href']
@@ -22,6 +29,7 @@ def get_next_page(list_li, num):
 
 
 def change_flors(string):
+    """Функция возвращает целочисленное значение для этажа"""
     floor = ""
     for i in string:
         if i == " ":
@@ -31,6 +39,10 @@ def change_flors(string):
 
 
 def area(string):
+    """Функция возвращает площадь в виде числа с плавающей точкой
+
+       С помощью регулярных выражений находится число в строке, затем с помощью цикла расширение Га или А. Затем
+       сравнивая полученное занчение А или Га и домножаем на необходимый множитель"""
     import re
 
     strnum = re.sub(r'[^\d.]', '', string.replace(",", "."))
@@ -55,6 +67,10 @@ def area(string):
 
 
 def make_parameters(result, objects):
+    """Функция определяет параметры для результирующего словаря
+
+       С помощью цикла и условий определяются искомые параметры для результирующего словаря. Если в цикл попадает объект
+       с именем div, то он переопределяется с помощью поиска в объекте div с классом (a10a3f92e9--text--eplgM)"""
     for object in objects:
         if object.name == "div":
             object = object.find("div", {"class": "a10a3f92e9--text--eplgM"})
@@ -80,33 +96,58 @@ def make_parameters(result, objects):
                 result['area'] = area(object.contents[1].text)
 
 
+def pars_address(additional_div, result):
+    """Эта функция определяет адрес для записи в результирующем словаре"""
+    result['address'] = {}
+    all_a = additional_div.find_all('a')
+    result['address']['region'] = all_a[0].text
+    result['address']['city'] = all_a[1].text
+    result['address']['district'] = all_a[2].text
+    result['address']['street'] = all_a[3].text
+    result['address']['house'] = all_a[4].text
+
+
 def parse_ad(url):
+    """Функция создает и определяет словарь с параметрами для отдельной записи в конечном словаре
+
+       Сперва с помощью requests получаем уро. Затем с помощью BeautifulSoup начинаем парсинг. Далее с помощью метода
+       find_all находим все li содержащие параметры и они определяются в словарь. После подобные манипуляции проводятся
+       с div и дополнительными параметрами. В конце определятся цена записи"""
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get(url) as response:
     while True:
         response = requests.get(url)
         if response.status_code == 200:
-            break
+            result = {}
 
-    result = {}
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+            all_li = soup.find_all('li', {'class': 'a10a3f92e9--item--jW0Mi a10a3f92e9--item--hm6MM',
+                                      'data-name': 'AdditionalFeatureItem'})
+            make_parameters(result, all_li)
 
-    all_li = soup.find_all('li', {'class': 'a10a3f92e9--item--jW0Mi a10a3f92e9--item--hm6MM',
-                              'data-name': 'AdditionalFeatureItem'})
-    make_parameters(result, all_li)
+            divs = soup.find_all('div', {'class': 'a10a3f92e9--item--Jp5Qv', 'data-name': 'ObjectFactoidsItem'})
+            make_parameters(result, divs)
 
-    divs = soup.find_all('div', {'class': 'a10a3f92e9--item--Jp5Qv', 'data-name': 'ObjectFactoidsItem'})
-    make_parameters(result, divs)
+            additional_div = soup.find_all('div', {"data-name": "TechnicalCharacter",
+                                                   "class": "a10a3f92e9--container--tu25B"})
+            if additional_div:
+                result["additional_info"] = []
+                for div in additional_div[0]:
+                    result["additional_info"].append(div.text)
 
-    additional_div = soup.find_all('div', {"data-name": "TechnicalCharacter",
-                                           "class": "a10a3f92e9--container--tu25B"})
-    if additional_div:
-        result["additional_info"] = []
-        for div in additional_div[0]:
-            result["additional_info"].append(div.text)
+            address_div = soup.find('div', {'data-name': 'AddressContainer'})
+            pars_address(address_div, result)
 
-    return result
+            cost = soup.find('div', {'data-testid': 'price-amount', "class": 'a10a3f92e9--amount--ON6i1'})
+            result['cost'] = cost.contents[0].text[:-1]
+
+            return result
 
 def parse_page(url, result, num = 2):
+    """Функция парсит основную страницу и вызывает функцию, которая парсит адрес
+
+       После того как пропарсятся все адреса вызывается эта же функция, но уже со следующей страницы"""
     while True:
         response = requests.get(url)
         if response.status_code == 200:
@@ -125,21 +166,25 @@ def parse_page(url, result, num = 2):
         result[f'{a["href"]}'] = parameters
 
     div_pagination = soup.find('div', {'data-name': 'Pagination'})
-    next_url = get_next_page(div_pagination.find_all_next('li'), num)
-    if next_url:
-        parse_page(next_url, result, num + 1)
-
-    with open(f"json{num}.txt", 'w') as file:
-        json.dump(result, file, indent=4)
-
+    # next_url = get_next_page(div_pagination.find_all_next('li'), num)
+    # if next_url:
+    #     parse_page(next_url, result, num + 1)
+    #
+    # with open(f"json{num}.txt", 'w') as file:
+    #     json.dump(result, file, indent=4)
+    return result
 
 def main_function():
-    num = 1
+    """Основная функция, которая запускает парсинг"""
+    n = 0
     for url in urls:
-        res = {}
-        parse_page(url, res)
-        num += 1
+        res = parse_page(url, {})
+        dir = os.path.abspath(__file__).replace('parser_for_1_kurs_2_sim\parser_cian.py',
+                                                f'project_1_kurs_2_sim\cache\cian\json_{n}.json',
+                                                1)
+        with open(dir, "w") as file:
+            json.dump(res, file, indent=4)
 
-
+# Условие позволяющие запускать код из терминала
 if __name__ == "__main__":
     main_function()
