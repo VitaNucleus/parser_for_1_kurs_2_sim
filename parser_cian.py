2fc4c6c30889bf90a0e9b98d987e7c05414f6a5a
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2024-05-11 13:52:36 +0500

    11.05.2024 D забыл раскомментировать

--- parser_cian.py
+++ parser_cian.py
@@ -8,10 +8,10 @@ import re
 
 load_dotenv()
 # Урлы, по которым будет производиться поиск адресов с параметрами
-urls = [#'https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&'
-        #'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',
-        #'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
-        #'1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
+urls = ['https://perm.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=offices&office_type%5B0%5D=1&'
+        'office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&office_type%5B4%5D=11&region=4927',
+        'https://perm.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D='
+        '1&office_type%5B1%5D=2&office_type%5B2%5D=3&office_type%5B3%5D=5&region=4927',
         'https://perm.cian.ru/snyat-kommercheskiy-uchastok/',
         'https://perm.cian.ru/kupit-kommercheskiy-uchastok/']
 
